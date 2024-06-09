7706a9d1e04074eac122c2e5d435f4bc25f05b41
hammad-119 <bscs2012119@szabist.pk>
2024-06-09 21:43:21 +0500

    Update cloudinary.js

--- services/production/utilities/cloudinary.js
+++ services/production/utilities/cloudinary.js
@@ -2,8 +2,6 @@ const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
   cloud_name: "dskffiksr",
-  api_key: "424982255271138",
-  api_secret: "P4Z5k3-0QouAnyVou8EA74g33Es",
 });
 async function uploadImage(base64Data, folderName) {
   try {
